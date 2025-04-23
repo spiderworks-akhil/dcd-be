a856ddb0db86298966db51da25c97976c73dfafc
supal-s <supal@spiderworks.in>
2025-04-23 12:21:08 +0530

    update in event detail page

--- app/Http/Controllers/Apis/EventController.php
+++ app/Http/Controllers/Apis/EventController.php
@@ -12,11 +12,12 @@ use App\Http\Resources\CategoryCollection;
 
 class EventController extends Controller
 {
-    public function index(Request $request){
-        try{
+    public function index(Request $request)
+    {
+        try {
             $data = $request->all();
-            $limit = !empty($data['limit'])?(int)$data['limit']:10;
-            $type = !empty($data['language'])?$data['language']:"en";
+            $limit = !empty($data['limit']) ? (int)$data['limit'] : 10;
+            $type = !empty($data['language']) ? $data['language'] : "en";
             $events = Event::where('status', 1);
             if (!empty($data['category_id'])) {
                 $events = $event->whereHas('categories', function ($query) use ($data) {
@@ -28,45 +29,46 @@ class EventController extends Controller
             } elseif (!empty($data['filter']) && $data['filter'] === 'past') {
                 $events = $events->where('end_time', '<', now());
             }
-            $events = $events->where('type',$type)->orderBy('start_time','DESC')->paginate($limit);
+            $events = $events->where('type', $type)->orderBy('start_time', 'DESC')->paginate($limit);
             return new EventListingCollection($events);
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
 
-    public function featured(Request $request){
+    public function featured(Request $request)
+    {
         $data = $request->all();
-        $type = !empty($data['language'])?$data['language']:"en";
+        $type = !empty($data['language']) ? $data['language'] : "en";
         $events = Event::where('status', 1)
             ->where('type', $type)
             ->where('is_featured', 1);
 
-            if (!empty($data['category_id'])) {
-                $events = $events->whereHas('categories', function ($query) use ($data) {
-                    $query->where('id', $data['category_id']);
-                });
-            }
+        if (!empty($data['category_id'])) {
+            $events = $events->whereHas('categories', function ($query) use ($data) {
+                $query->where('id', $data['category_id']);
+            });
+        }
 
-            if (!empty($data['filter']) && $data['filter'] === 'upcoming') {
-                $events = $events->where('start_time', '>', now());
-            } elseif (!empty($data['filter']) && $data['filter'] === 'past') {
-                $events = $events->where('end_time', '<', now());
-            }
-    
-            $events = $events->orderBy('priority', 'DESC')
+        if (!empty($data['filter']) && $data['filter'] === 'upcoming') {
+            $events = $events->where('start_time', '>', now());
+        } elseif (!empty($data['filter']) && $data['filter'] === 'past') {
+            $events = $events->where('end_time', '<', now());
+        }
+
+        $events = $events->orderBy('priority', 'DESC')
             ->get();
         return new EventListingCollection($events);
     }
 
-    public function view(Request $request, $slug){
-        try{
+    public function view(Request $request, $slug)
+    {
+        try {
             $data = $request->all();
-            $type = !empty($data['language'])?$data['language']:"en";
-            $event = Event::where('slug', $slug)->where('type',$type)->where('status', 1)->first();
-            if(!$event)
+            $type = !empty($data['language']) ? $data['language'] : "en";
+            $event = Event::where('slug', $slug)->where('type', $type)->where('status', 1)->first();
+            if (!$event)
                 return response()->json(['error' => 'Not found'], 404);
 
             $event->related_event = Event::where('id', '!=', $event->id)
@@ -75,14 +77,14 @@ class EventController extends Controller
                 ->take(5)
                 ->get();
             return new EventResource($event);
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
-    public function categories(Request $request){
-        try{
+    public function categories(Request $request)
+    {
+        try {
             $data = $request->all();
             $categories = Category::where('status', 1)
                 ->where('category_type', 'Event')
@@ -111,9 +113,8 @@ class EventController extends Controller
             });
 
             return new CategoryCollection($formattedCategories);
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
-
+    }
 }
