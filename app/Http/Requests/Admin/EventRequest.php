958b0d4426b5d53a50adfe62bf556e170d06911d
supal-s <supal@spiderworks.in>
2025-04-23 14:15:06 +0530

    events static page update

--- app/Http/Requests/Admin/EventRequest.php
+++ app/Http/Requests/Admin/EventRequest.php
@@ -24,7 +24,7 @@ class EventRequest extends FormRequest
         $ignoreId = ($this->input('id'))?decrypt($this->input('id')):NULL;
         return [
             'name' => 'required|max:250',
-            'slug' => "required|max:250|unique:events,slug,{$ignoreId},id,deleted_at,NULL",
+            'slug' => "required|max:250|unique:events,slug,{$ignoreId},id,deleted_at,NULL,type,{$this->input('type')}",
         ];
     }
 }
