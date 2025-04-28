65761b0aa786cab0021bcdf69ea421c38cc1870f
supal-s <supal@spiderworks.in>
2025-04-28 17:13:26 +0530

    chnages in widget section

--- resources/views/admin/widget_ar.blade.php
+++ resources/views/admin/widget_ar.blade.php
@@ -204,3 +204,47 @@
                 </form>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <form method="POST" action="{{ route('admin.widgets.save') }}" class="p-t-15" id="InputFrm" data-validate=true>
+                    @csrf
+                    <input type="hidden" name="id" value="10">
+                    <div class="card">
+                        <div class="card-header">
+                            Join The Club
+                        </div>
+                        <div class="card-body row">
+                            <div class="col-md-12">
+                                <div class="row" style="text-align: center;">
+                                    <div class="form-group col-md-12">
+                                        Title
+                                        <input type="text" name="section[title]" class="form-control"
+                                            value="@if(!empty($data['join_the_club']['title'])){{$data['join_the_club']['title']}} @endif">
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        Short Description
+                                        <textarea name="section[short_description]" cols="30" rows="2" class="form-control">@if(!empty($data['join_the_club']['short_description'])){{$data['join_the_club']['short_description']}} @endif</textarea>
+                                    </div>
+                                    <div class="row">
+                                    <div class="form-group col-md-6">
+                                        Button Text
+                                        <input type="text" name="section[button_text]" class="form-control"
+                                            value="@if(!empty($data['join_the_club']['button_text'])){{$data['join_the_club']['button_text']}} @endif">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        Button Link
+                                        <input type="text" name="section[button_link]" class="form-control"
+                                            value="@if(!empty($data['join_the_club']['button_link'])){{$data['join_the_club']['button_link']}} @endif">
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-sm btn-primary">Update</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
