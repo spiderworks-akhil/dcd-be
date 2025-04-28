25f8a60ade379557901f2cb4d1f140f0fd38eee3
supal-s <supal@spiderworks.in>
2025-04-28 12:45:36 +0530

    updates in cats in progress"

---
+++ resources/views/admin/static_pages/_partials/inxex.blade.php
@@ -0,0 +1,83 @@
+
+<div id="form-vertical" class="form-horizontal form-wizard-wrapper">
+
+<h3>Banner Content</h3>
+<fieldset>
+ 
+    <div class="form-group col-md-12">
+        @if ($obj->type == 'en')
+            <a href="{{route('admin.sliders.edit',[1])}}" class="btn btn-sm btn-danger" target="_blank"> Add Sliders </a>
+        @else
+            <a href="{{route('admin.sliders.edit',[2])}}" class="btn btn-sm btn-danger" target="_blank"> Add Sliders </a>
+        @endif
+    </div>
+</fieldset>
+
+<h3>Middle Content</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Title</label>
+        <input type="text" name="content[title_2]" class="form-control"
+            @if($obj->content && isset($obj->content['title_2']))
+                value="{{ $obj->content['title_2'] }}"
+            @endif
+        >
+    </div>
+    <div class="form-group col-md-12">
+        <label>Content</label>
+        <textarea name="content[description_2]" class="form-control editor">
+            @if($obj->content && isset($obj->content['description_2']))
+                {{ $obj->content['description_2'] }}
+            @endif
+        </textarea>
+    </div>
+    <div class="form-group">
+        @php
+            $media_id_2 = ($obj->content && isset($obj->content['media_id_2'])) ? $obj->content['media_id_2'] : null;
+        @endphp
+        @include('admin.media.set_file', [
+            'file' => $media_id_2,
+            'title' => 'Featured Image',
+            'popup_type' => 'single_image',
+            'type' => 'Image',
+            'holder_attr' => 'content[media_id_2]',
+            'id' => 'content_image_2',
+            'display' => 'horizontal'
+        ])
+    </div>
+</fieldset>
+
+<h3>Bottom Content</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Title</label>
+        <input type="text" name="content[title_3]" class="form-control"
+            @if($obj->content && isset($obj->content['title_3']))
+                value="{{ $obj->content['title_3'] }}"
+            @endif
+        >
+    </div>
+    <div class="form-group col-md-12">
+        <label>Content</label>
+        <textarea name="content[description_3]" class="form-control editor">
+            @if($obj->content && isset($obj->content['description_3']))
+                {{ $obj->content['description_3'] }}
+            @endif
+        </textarea>
+    </div>
+    <div class="form-group">
+        @php
+            $media_id_3 = ($obj->content && isset($obj->content['media_id_3'])) ? $obj->content['media_id_3'] : null;
+        @endphp
+        @include('admin.media.set_file', [
+            'file' => $media_id_3,
+            'title' => 'Featured Image',
+            'popup_type' => 'single_image',
+            'type' => 'Image',
+            'holder_attr' => 'content[media_id_3]',
+            'id' => 'content_image_3',
+            'display' => 'horizontal'
+        ])
+    </div>
+</fieldset>
+</div>
