d7cb7cff027764cffa98f5d06dcfc7be917e382e
supal-s <supal@spiderworks.in>
2025-04-28 19:08:23 +0530

    updates in cats in progress"

--- resources/views/admin/static_pages/_partials/index.blade.php
+++ resources/views/admin/static_pages/_partials/index.blade.php
@@ -105,4 +105,79 @@
             <a href="{{route('admin.services.index',['type'=>$obj->type])}}" class="btn btn-sm btn-danger" target="_blank"> Add Divisions </a>
     </div>
 </fieldset>
+<h3>Social Section</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Title</label>
+        <input type="text" name="content[social_title]" class="form-control"
+            @if($obj->content && isset($obj->content['social_title']))
+                value="{{ $obj->content['social_title'] }}"
+            @endif
+        >
+    </div>
+    <div class="form-group col-md-12">
+        <label>Small text</label>
+        <textarea name="content[small_text]" class="form-control" rows="5">@if($obj->content && isset($obj->content['small_text'])){{ $obj->content['small_text'] }}@endif</textarea>
+    </div>
+
+   
+</fieldset>
+<h3>Awards Section</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Title</label>
+        <input type="text" name="content[awards_title]" class="form-control"
+            @if($obj->content && isset($obj->content['awards_title']))
+                value="{{ $obj->content['awards_title'] }}"
+            @endif
+        >
+    </div>
+    <div class="form-group col-md-12">
+        <label>Description</label>
+        <textarea name="content[awards_description]" class="form-control" rows="5">@if($obj->content && isset($obj->content['awards_description'])){{ $obj->content['awards_description'] }}@endif</textarea>
+    </div>
+    <div class="form-group col-md-12">
+        @if ($obj->type == 'en')
+            <input type="hidden" name="content[awards_listing_id]" value="4">
+            <a href="{{route('admin.listing-items.index',[4])}}" class="btn btn-sm btn-danger" target="_blank"> Add Awards </a>
+        @else
+            <input type="hidden" name="content[awards_listing_id]" value="5">
+            <a href="{{route('admin.listing-items.index',[5])}}" class="btn btn-sm btn-danger" target="_blank"> Add Awards </a>
+        @endif
+    </div>
+</fieldset>
+<h3>News Section</h3>
+<fieldset>
+    <div class="form-group col-md-12">
+        <label>Title</label>
+        <input type="text" name="content[news_title]" class="form-control"
+            @if($obj->content && isset($obj->content['news_title']))
+                value="{{ $obj->content['news_title'] }}"
+            @endif
+        >
+    </div>
+    <div class="form-group col-md-12">
+        <label>Description</label>
+        <textarea name="content[news_description]" class="form-control" rows="5">@if($obj->content && isset($obj->content['news_description'])){{ $obj->content['news_description'] }}@endif</textarea>
+    </div>
+    <div class="form-group col-md-6">
+        <label>Button Text</label>
+        <input type="text" name="content[news_btn_text]" class="form-control"
+            @if($obj->content && isset($obj->content['news_btn_text']))
+                value="{{ $obj->content['news_btn_text'] }}"
+            @endif
+        >
+    </div>
+    <div class="form-group col-md-6">
+        <label>Button Link</label>
+        <input type="text" name="content[news_btn_link]" class="form-control"
+            @if($obj->content && isset($obj->content['news_btn_link']))
+                value="{{ $obj->content['news_btn_link'] }}"
+            @endif
+        >
+    </div>
+        <a href="{{route('admin.news.index')}}" class="btn btn-sm btn-danger" target="_blank"> Add News </a>
+    
+    </div>
+</fieldset>
 </div>
