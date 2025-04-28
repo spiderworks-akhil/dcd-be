662f21658f0fd995a24e389bc2abbd0b0bd78040
supal-s <supal@spiderworks.in>
2025-04-28 13:21:15 +0530

    updates in cats in progress"

--- app/Http/Resources/SliderMedia.php
+++ app/Http/Resources/SliderMedia.php
@@ -14,6 +14,6 @@ class SliderMedia extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [new Media($this->media)];
     }
 }
