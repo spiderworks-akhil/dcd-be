7e553230c81f34db4d82278c2bc8ea0b96b3fb8c
supal-s <supal@spiderworks.in>
2025-04-28 13:15:45 +0530

    updates in cats in progress"

--- app/Http/Resources/FrontendPage.php
+++ app/Http/Resources/FrontendPage.php
@@ -159,7 +159,7 @@ class FrontendPage extends JsonResource
         $out = new \App\Models\Slider();
         if ($language == 'en') {
             $out = $out->find(2);
-        } else {
+        } else if ($language == 'ar') {
             $out = $out->find(3);
         }
         return $out;
