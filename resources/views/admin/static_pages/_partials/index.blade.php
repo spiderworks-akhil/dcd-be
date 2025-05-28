75395008b19c42809446a602f0a1a9635e44d362
seethalSpiderWorks <seethalbabu213@gmail.com>
2025-05-28 11:05:20 +0530

    social section in index

--- resources/views/admin/static_pages/_partials/index.blade.php
+++ resources/views/admin/static_pages/_partials/index.blade.php
@@ -2,7 +2,7 @@
 <div id="form-vertical" class="form-horizontal form-wizard-wrapper">
 <h3>Banner Content</h3>
 <fieldset>
- 
+
     <div class="form-group col-md-12">
         @if ($obj->type == 'en')
             <a href="{{route('admin.sliders.edit',[encrypt(1)])}}" class="btn btn-sm btn-danger" target="_blank"> Add Sliders </a>
@@ -116,7 +116,7 @@
             >
         </div>
     </div>
-    
+
     <div class="form-group col-md-12">
             <a href="{{route('admin.services.index',['type'=>$obj->type])}}" class="btn btn-sm btn-danger" target="_blank"> Add Divisions </a>
     </div>
@@ -136,7 +136,243 @@
         <textarea name="content[small_text]" class="form-control" rows="5">@if($obj->content && isset($obj->content['small_text'])){{ $obj->content['small_text'] }}@endif</textarea>
     </div>
 
-   
+    <div class="row">
+
+        <div class="form-group col-md-4">
+            <label>Video Description</label>
+            <input type="text" name="content[video_description_1]" class="form-control"
+                @if($obj->content && isset($obj->content['video_description_1']))
+                    value="{{ $obj->content['video_description_1'] }}"
+                @endif
+            >
+        </div>
+        <div class="form-group col-md-4">
+                @php
+                    $media_id_1 = $obj->content && isset($obj->content['media_id_1']) ? $obj->content['media_id_1'] : null;
+                @endphp
+                @include('admin.media.set_file', [
+                    'file' => $media_id_1,
+                    'title' => 'Video',
+                    'popup_type' => 'single_video',
+                    'type' => 'Video',
+                    'holder_attr' => 'content[media_id_1]',
+                    'id' => 'content_video_1',
+                    'display' => 'horizontal',
+                ])
+        </div>
+
+         <div class="form-group col-md-4">
+            <label>Link 1</label>
+            <input type="text" name="content[link_1]" class="form-control"
+                @if($obj->content && isset($obj->content['link_1']))
+                    value="{{ $obj->content['link_1'] }}"
+                @endif
+            >
+        </div>
+
+        <div class="form-group col-md-6">
+            <label>Image title</label>
+            <input type="text" name="content[image_title_1]" class="form-control"
+                @if($obj->content && isset($obj->content['image_title_1']))
+                    value="{{ $obj->content['image_title_1'] }}"
+                @endif
+            >
+        </div>
+        <div class="form-group  col-md-6">
+            <label>Image 1</label>
+
+            @php
+                $media_id_2 = $obj->content && isset($obj->content['media_id_2']) ? $obj->content['media_id_2'] : null;
+            @endphp
+            @include('admin.media.set_file', [
+                'file' => $media_id_2,
+                'title' => 'Image',
+                'popup_type' => 'single_image',
+                'type' => 'Image',
+                'holder_attr' => 'content[media_id_2]',
+                'id' => 'content_image_2',
+                'display' => 'horizontal',
+            ])
+        </div>
+
+         <div class="form-group col-md-6">
+            <label>Image Short Description</label>
+            <input type="text" name="content[image_short_description_1]" class="form-control"
+                @if($obj->content && isset($obj->content['image_short_description_1']))
+                    value="{{ $obj->content['image_short_description_1'] }}"
+                @endif
+            >
+        </div>
+
+          <div class="form-group col-md-6">
+            <label>Link 2</label>
+            <input type="text" name="content[link_2]" class="form-control"
+                @if($obj->content && isset($obj->content['link_2']))
+                    value="{{ $obj->content['link_2'] }}"
+                @endif
+            >
+        </div>
+
+         <div class="form-group col-md-4">
+            <label>Image title </label>
+            <input type="text" name="content[image_title_2]" class="form-control"
+                @if($obj->content && isset($obj->content['image_title_2']))
+                    value="{{ $obj->content['image_title_2'] }}"
+                @endif
+            >
+        </div>
+        <div class="form-group  col-md-4">
+            <label>Image 2</label>
+
+            @php
+                $media_id_3 = $obj->content && isset($obj->content['media_id_3']) ? $obj->content['media_id_3'] : null;
+            @endphp
+            @include('admin.media.set_file', [
+                'file' => $media_id_3,
+                'title' => 'Image',
+                'popup_type' => 'single_image',
+                'type' => 'Image',
+                'holder_attr' => 'content[media_id_3]',
+                'id' => 'content_image_3',
+                'display' => 'horizontal',
+            ])
+        </div>
+
+          <div class="form-group col-md-4">
+            <label>Link 3</label>
+            <input type="text" name="content[link_3]" class="form-control"
+                @if($obj->content && isset($obj->content['link_3']))
+                    value="{{ $obj->content['link_3'] }}"
+                @endif
+            >
+        </div>
+
+         <div class="form-group  col-md-6">
+            <label>Image 3</label>
+
+            @php
+                $media_id_4 = $obj->content && isset($obj->content['media_id_4']) ? $obj->content['media_id_4'] : null;
+            @endphp
+            @include('admin.media.set_file', [
+                'file' => $media_id_4,
+                'title' => 'Image',
+                'popup_type' => 'single_image',
+                'type' => 'Image',
+                'holder_attr' => 'content[media_id_4]',
+                'id' => 'content_image_4',
+                'display' => 'horizontal',
+            ])
+        </div>
+
+          <div class="form-group col-md-6">
+            <label>Link 4</label>
+            <input type="text" name="content[link_4]" class="form-control"
+                @if($obj->content && isset($obj->content['link_4']))
+                    value="{{ $obj->content['link_4'] }}"
+                @endif
+            >
+        </div>
+
+         <div class="form-group col-md-4">
+            <label>Image title </label>
+            <input type="text" name="content[image_title_4]" class="form-control"
+                @if($obj->content && isset($obj->content['image_title_4']))
+                    value="{{ $obj->content['image_title_4'] }}"
+                @endif
+            >
+        </div>
+        <div class="form-group  col-md-4">
+            <label>Image 4</label>
+
+            @php
+                $media_id_5 = $obj->content && isset($obj->content['media_id_5']) ? $obj->content['media_id_5'] : null;
+            @endphp
+            @include('admin.media.set_file', [
+                'file' => $media_id_5,
+                'title' => 'Image',
+                'popup_type' => 'single_image',
+                'type' => 'Image',
+                'holder_attr' => 'content[media_id_5]',
+                'id' => 'content_image_5',
+                'display' => 'horizontal',
+            ])
+        </div>
+
+          <div class="form-group col-md-4">
+            <label>Link 5</label>
+            <input type="text" name="content[link_5]" class="form-control"
+                @if($obj->content && isset($obj->content['link_5']))
+                    value="{{ $obj->content['link_5'] }}"
+                @endif
+            >
+        </div>
+
+
+        <div class="form-group  col-md-6">
+            <label>Image 5</label>
+
+            @php
+                $media_id_6 = $obj->content && isset($obj->content['media_id_6']) ? $obj->content['media_id_6'] : null;
+            @endphp
+            @include('admin.media.set_file', [
+                'file' => $media_id_6,
+                'title' => 'Image',
+                'popup_type' => 'single_image',
+                'type' => 'Image',
+                'holder_attr' => 'content[media_id_6]',
+                'id' => 'content_image_6',
+                'display' => 'horizontal',
+            ])
+        </div>
+
+          <div class="form-group col-md-6">
+            <label>Link 6</label>
+            <input type="text" name="content[link_6]" class="form-control"
+                @if($obj->content && isset($obj->content['link_6']))
+                    value="{{ $obj->content['link_6'] }}"
+                @endif
+            >
+        </div>
+
+           <div class="form-group col-md-4">
+            <label>Image title </label>
+            <input type="text" name="content[image_title_6]" class="form-control"
+                @if($obj->content && isset($obj->content['image_title_6']))
+                    value="{{ $obj->content['image_title_6'] }}"
+                @endif
+            >
+        </div>
+        <div class="form-group  col-md-4">
+            <label>Image 6</label>
+
+            @php
+                $media_id_7 = $obj->content && isset($obj->content['media_id_7']) ? $obj->content['media_id_7'] : null;
+            @endphp
+            @include('admin.media.set_file', [
+                'file' => $media_id_7,
+                'title' => 'Image',
+                'popup_type' => 'single_image',
+                'type' => 'Image',
+                'holder_attr' => 'content[media_id_7]',
+                'id' => 'content_image_7',
+                'display' => 'horizontal',
+            ])
+        </div>
+
+          <div class="form-group col-md-4">
+            <label>Link 7</label>
+            <input type="text" name="content[link_7]" class="form-control"
+                @if($obj->content && isset($obj->content['link_7']))
+                    value="{{ $obj->content['link_7'] }}"
+                @endif
+            >
+        </div>
+
+
+    </div>
+
+
+
 </fieldset>
 <h3>Awards Section</h3>
 <fieldset>
@@ -160,7 +396,7 @@
             <input type="hidden" name="content[awards_listing_id]" value="5">
             <a href="{{route('admin.listing-items.index',[5])}}" class="btn btn-sm btn-danger" target="_blank"> Add Awards </a>
         @endif
-        
+
     </div>
     <div class="row">
         <div class="form-group col-md-6">
@@ -212,6 +448,7 @@
         >
     </div>
         <a href="{{route('admin.news.index')}}" class="btn btn-sm btn-danger" target="_blank"> Add News </a>
-    
+
 </fieldset>
+
 </div>
