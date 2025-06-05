5705645f454afa2b3a4e546625bc95340d5a98e2
seethalSpiderWorks <seethalbabu213@gmail.com>
2025-06-05 17:36:57 +0530

    terms and condition changes

--- resources/views/admin/static_pages/_partials/terms-and-conditions.blade.php
+++ resources/views/admin/static_pages/_partials/terms-and-conditions.blade.php
@@ -33,74 +33,25 @@
                         'display' => 'horizontal'
                     ])
                 </div>
-            </fieldset>
 
-            <h3>Middle Content</h3>
-            <fieldset>
-                <div class="form-group col-md-12">
-                    <label>Title</label>
-                    <input type="text" name="content[title_2]" class="form-control"
-                        @if($obj->content && isset($obj->content['title_2']))
-                            value="{{ $obj->content['title_2'] }}"
+                 <div class="form-group col-md-12">
+                    <label>Email</label>
+                    <input type="text" name="content[email]" class="form-control"
+                        @if($obj->content && isset($obj->content['email']))
+                            value="{{ $obj->content['email'] }}"
                         @endif
                     >
                 </div>
-                <div class="form-group col-md-12">
-                    <label>Content</label>
-                    <textarea name="content[description_2]" class="form-control editor">
-                        @if($obj->content && isset($obj->content['description_2']))
-                            {{ $obj->content['description_2'] }}
-                        @endif
-                    </textarea>
-                </div>
-                <div class="form-group">
-                    @php
-                        $media_id_2 = ($obj->content && isset($obj->content['media_id_2'])) ? $obj->content['media_id_2'] : null;
-                    @endphp
-                    @include('admin.media.set_file', [
-                        'file' => $media_id_2,
-                        'title' => 'Featured Image',
-                        'popup_type' => 'single_image',
-                        'type' => 'Image',
-                        'holder_attr' => 'content[media_id_2]',
-                        'id' => 'content_image_2',
-                        'display' => 'horizontal'
-                    ])
-                </div>
-            </fieldset>
 
-            <h3>Bottom Content</h3>
-            <fieldset>
-                <div class="form-group col-md-12">
-                    <label>Title</label>
-                    <input type="text" name="content[title_3]" class="form-control"
-                        @if($obj->content && isset($obj->content['title_3']))
-                            value="{{ $obj->content['title_3'] }}"
+                 <div class="form-group col-md-12">
+                    <label>Short Description</label>
+                    <input type="text" name="content[short_description]" class="form-control"
+                        @if($obj->content && isset($obj->content['short_description']))
+                            value="{{ $obj->content['short_description'] }}"
                         @endif
                     >
                 </div>
-                <div class="form-group col-md-12">
-                    <label>Content</label>
-                    <textarea name="content[description_3]" class="form-control editor">
-                        @if($obj->content && isset($obj->content['description_3']))
-                            {{ $obj->content['description_3'] }}
-                        @endif
-                    </textarea>
-                </div>
-                <div class="form-group">
-                    @php
-                        $media_id_3 = ($obj->content && isset($obj->content['media_id_3'])) ? $obj->content['media_id_3'] : null;
-                    @endphp
-                    @include('admin.media.set_file', [
-                        'file' => $media_id_3,
-                        'title' => 'Featured Image',
-                        'popup_type' => 'single_image',
-                        'type' => 'Image',
-                        'holder_attr' => 'content[media_id_3]',
-                        'id' => 'content_image_3',
-                        'display' => 'horizontal'
-                    ])
-                </div>
             </fieldset>
+
+
         </div>
-        
