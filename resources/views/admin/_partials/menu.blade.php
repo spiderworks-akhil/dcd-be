6368541eef24d1cd01e4d4b07ccfd7fe74b714b0
seethalSpiderWorks <seethal@spiderworks.in>
2025-12-29 13:56:08 +0530

    changes in menus

--- resources/views/admin/_partials/menu.blade.php
+++ resources/views/admin/_partials/menu.blade.php
@@ -8,17 +8,19 @@
 
     @endphp
     @if(auth('admin')->user()->can($item->permission_name))
-    <li class="{{$class}}">
-        <a @if(isset($item->children)) href="#" @else href="{{route($item->route)}}" @endif @if($depth>0) class="nav-link" @endif> {!! stripslashes($item->icon) !!}<span>{{$item->name}}</span>
-            @if(isset($item->children))
-                <span class="menu-arrow"><i class="mdi mdi-chevron-right"></i></span>
-            @endif
-        </a>
-        @if(isset($item->children))
-            <ul class="nav-second-level" aria-expanded="false">
-                @include('admin._partials.menu', ['menu_items'=>$item->children, 'parent'=>$parent, 'cur_url'=>$cur_url, 'depth'=>$depth++])
-            </ul>
+        @if($item->name !== 'Settings' || auth('admin')->user()->hasRole('Admin'))
+            <li class="{{$class}}">
+                <a @if(isset($item->children)) href="#" @else href="{{route($item->route)}}" @endif @if($depth>0) class="nav-link" @endif> {!! stripslashes($item->icon) !!}<span>{{$item->name}}</span>
+                    @if(isset($item->children))
+                        <span class="menu-arrow"><i class="mdi mdi-chevron-right"></i></span>
+                    @endif
+                </a>
+                @if(isset($item->children))
+                    <ul class="nav-second-level" aria-expanded="false">
+                        @include('admin._partials.menu', ['menu_items'=>$item->children, 'parent'=>$parent, 'cur_url'=>$cur_url, 'depth'=>$depth++])
+                    </ul>
+                @endif
+            </li>
         @endif
-    </li>
     @endif
 @endforeach  
